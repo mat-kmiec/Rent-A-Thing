ed89b16fed81d70b43f3d30e27110b7542b46eb9
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2026-01-16 00:45:30 +0100

    feat(rentals): implement entities with liquibase changelog

---
+++ src/main/java/pl/rentathing/Rental/Entity/PaymentMethod.java
@@ -0,0 +1,5 @@
+package pl.rentathing.Rental.Entity;
+
+public enum PaymentMethod {
+    CARD, BLIK, CASH_ON_PICKUP
+}
---
+++ src/main/java/pl/rentathing/Rental/Entity/Rental.java
@@ -0,0 +1,85 @@
+package pl.rentathing.Rental.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import pl.rentathing.item.entity.Item;
+import pl.rentathing.user.entity.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "rentals")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @NotNull
+    private LocalDateTime startDateTime;
+
+    @NotNull
+    private LocalDateTime endDateTime;
+
+    private LocalDateTime returnDateTime;
+
+    @PositiveOrZero
+    @Column(precision = 10, scale = 2)
+    private BigDecimal totalCost;
+
+    @PositiveOrZero
+    @Column(precision = 10, scale = 2)
+    private BigDecimal deposit;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryMethod deliveryMethod;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Column(nullable = false)
+    private boolean depositPaid = false;
+
+    @Column(nullable = false)
+    private boolean invoiceRequested = false;
+
+    @PositiveOrZero
+    private BigDecimal shippingCost;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RentalStatus status = RentalStatus.PENDING;
+
+    @Column(length = 1024)
+    private String handOverNotes;
+
+    @Column(length = 1024)
+    private String returnNotes;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+
+
+
+
+
+
+}
